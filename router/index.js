224ca6966a9e6a96ba9c2bda251c6b1444a6d2d8
wuhu <wuhu@itcast.cn>
2019-07-02 08:55:26 +0800

    实现所有导航项的路由跳转

--- router/index.js
+++ router/index.js
@@ -19,6 +19,18 @@ router.get('/',pagesController.getIndexPage)
 
 // 后台管理页面，统一添加admin做为前缀
       .get('/admin',pagesController.getAdminPage)
+      .get('/admin/categories',pagesController.getCategoriesPage)
+      .get('/admin/comments',pagesController.getCommentsPage)
+      .get('/admin/login',pagesController.getLoginPage)
+      .get('/admin/nav-menus',pagesController.getNavMenusPage)
+      .get('/admin/password-reset',pagesController.getPasswordResetPage)
+      .get('/admin/post-add',pagesController.getPostAddPage)
+      .get('/admin/posts',pagesController.getPostsPage)
+      .get('/admin/profile',pagesController.getProfilePage)
+      .get('/admin/slides',pagesController.getSlidesPage)
+      .get('/admin/settings',pagesController.getSettingsPage)
+      .get('/admin/users',pagesController.getUsersPage)
+
 
 
 
